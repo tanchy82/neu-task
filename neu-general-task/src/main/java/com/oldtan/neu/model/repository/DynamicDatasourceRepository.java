d40f8838b33627f60a7ad8ebf33e4caedda41e1c
tanchy <tanchy82@126.com>
2021-01-13 16:32:12 +0800

    commit data source api function unit test first bugs modify.

--- neu-general-task/src/main/java/com/oldtan/neu/model/repository/DynamicDatasourceRepository.java
+++ neu-general-task/src/main/java/com/oldtan/neu/model/repository/DynamicDatasourceRepository.java
@@ -8,13 +8,20 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 /**
- * @Description: TODO
+ * @Description: DynamicDatasource Entity Repository
  * @Author: tanchuyue
  * @Date: 21-1-4
  */
 @Repository
 public interface DynamicDatasourceRepository extends JpaRepository<DynamicDatasource, String> {
 
+    /**
+     * Find DynamicDatasource entity object by condition
+     * @param dbUrl
+     * @param dbType
+     * @param dbUsername
+     * @return
+     */
     Optional<DynamicDatasource> findFirstByDbUrlAndDbTypeAndDbUsername(String dbUrl, String dbType, String dbUsername);
 
 }
