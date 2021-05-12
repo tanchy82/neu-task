3e26d6eb2899b95c410de807f3e9b885d3aa95a9
tanchy <tanchy82@126.com>
2021-05-12 09:16:41 +0800

    ws

---
+++ neu-demo1/src/main/java/com/oldtan/Demo1Application.java
@@ -0,0 +1,18 @@
+package com.oldtan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: TODO
+ * @Author: tanchuyue Expose
+ * @Date: 21-4-29
+ */
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
